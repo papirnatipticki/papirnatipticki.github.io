88fa075deab6500925946fdc2d51f29710618644
papirnatipticki <tanja.spes@gmail.com>
2019-04-18 23:13:30 +0200

    Setup website layout & add initial stories

---
+++ js/script.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  // Ease in
+  $('body').animate({ opacity: 1 }, 700);
+
+  // Auto scroll
+  var up = $('.scroll-up');
+  var down = $('.scroll-down');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 400) {
+      up.addClass('show');
+    } else {
+      up.removeClass('show');
+    }
+  });
+
+  up.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 300);
+  });
+
+  down.on('click', function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 300);
+  });
+
+  // =========================================================================================
+  $('body').bind('copy', function (e) { e.preventDefault(); return false; });
+  $('body').bind('paste', function (e) { e.preventDefault(); return false; });
+  $('img').mousedown(function (e) { if (e.button == 2) { return false; } });
+  $('a.temp').click(function (e) { e.preventDefault(); });
+
+}());
